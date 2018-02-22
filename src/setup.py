d6e2d73204de19a45a2ec7d34368892fd53cd301
Emlyn O'Regan <emlynoregan@gmail.com>
2018-02-22 15:26:29 +1030

    fixes

--- src/setup.py
+++ src/setup.py
@@ -8,15 +8,15 @@ except ImportError:
 
 dist = setup(
     name='im_task_webapp2',
-    version='0.0.1dev1',
+    version='0.1.0',
     description='webapp2 handlers for @task. Use this in your webapp2 app. Also supports webapp',
     author='Emlyn O\'Regan',
     author_email='emlynoregan@gmail.com',
     url='https://github.com/emlynoregan/im_task_webapp2',
-    license='LICENSE.txt',
+    license='../LICENSE.txt',
     packages=['im_task_webapp2'],
     install_requires=['im_task'],
-    long_description=open('README.md').read(),
+    long_description=open('../README.md').read(),
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
